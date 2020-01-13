5d93e6208cc98116118d8b375b600c2f61257800
sssprogramer <sssprogramer@gmail.com>
2020-01-13 18:24:02 +0300

    add tests for utils

---
+++ EverySearch/Lib/Utils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EverySearch.Lib
+{
+    public class Utils
+    {
+        public struct Result<T> where T: class
+        {
+            public IList<Exception> Exceptions;
+            public T Value;
+        }
+
+        public static async Task<Result<T>> WaitAnySuccessful<T>(IList<Func<T>> funcs) where T: class{
+            int count = funcs.Count;
+            List<Task<T>> tasks = new List<Task<T>>();
+            BlockingCollection<Exception> exceptions = new BlockingCollection<Exception>();
+            foreach (var func in funcs)
+            {
+                tasks.Add(Task.Run(() => {
+                    try
+                    {
+                        var res = func();
+                        return res;
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                        return null;
+                    }
+                }));
+            }
+            while(tasks.Count > 0)
+            {
+                var task = await Task.WhenAny(tasks);
+                if (task.Result != null)
+                {
+                    return new Result<T>() { Value = task.Result, Exceptions = exceptions.ToArray() };
+                }
+                tasks.Remove(task);
+            }
+            return new Result<T>() { Value = null, Exceptions = exceptions.ToArray() };
+        }
+    }
+}
---
+++ EverySearchTests/UtilsTests.cs
@@ -0,0 +1,66 @@
+﻿using EverySearch.Lib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EverySearchTests
+{
+    [TestFixture]
+    class UtilsTests
+    {
+
+        [Test]
+        public async System.Threading.Tasks.Task WaitAnySuccessful_EmptyParameter_ReturnNullAsync()
+        {
+            IList<Func<string>> funcs = new List<Func<string>>();
+            var result = await Utils.WaitAnySuccessful(funcs);
+            Assert.IsNull(result.Value, "Result value is not null");
+            Assert.IsTrue(result.Exceptions.Count == 0, "Exceptions are not empty");
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task WaitAnySuccessful_AllFails_ReturnExceptions()
+        {
+            IList<Func<string>> funcs = new List<Func<string>>();
+            int n = 200;
+            for (int i = 0; i < n; i++)
+            {
+                funcs.Add(() => { throw new ArgumentException(); });
+            }
+            var result = await Utils.WaitAnySuccessful(funcs);
+            Assert.IsNull(result.Value, "Result value is not null");
+            Assert.IsTrue(result.Exceptions.Count == n, 
+                $"Method returned not all exceptions. Actual is {result.Exceptions.Count}, {n} expected");
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task WaitAnySuccessful_OneSuceed_ReturnValueAndExceptions()
+        {
+            IList<Func<string>> funcs = new List<Func<string>>()
+            {
+                () => { Thread.Sleep(50); return "Last task"; },
+                () => { throw new FormatException(); },
+                () => { throw new NullReferenceException(); }
+            };
+            var result = await Utils.WaitAnySuccessful(funcs);
+            Assert.IsTrue(result.Value == "Last task", "Result value is not correct");
+            Assert.IsTrue(result.Exceptions.Count == 2, "Wrong exceptions count");
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task WaitAnySuccessful_AllSuceed_ReturnValue()
+        {
+            IList<Func<string>> funcs = new List<Func<string>>()
+            {
+                () => {Thread.Sleep(2000);return "First task"; },
+                () => {Thread.Sleep(2000);return "Second task"; },
+                () => {return "Third task"; },
+            };
+            var result = await Utils.WaitAnySuccessful(funcs);
+            Assert.IsTrue(result.Value == "Third task", "Result value is null");
+            Assert.IsTrue(result.Exceptions.Count == 0, "Wrong exceptions count");
+        }
+    }
+}
